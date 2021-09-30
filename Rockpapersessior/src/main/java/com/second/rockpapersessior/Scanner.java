27201ce0197db21860d886350801ab09cc6d68dc
Ssoniakohli <87672946+Ssoniakohli@users.noreply.github.com>
2021-09-30 16:42:21 -0600

    Add files via upload

---
+++ Rockpapersessior/src/main/java/com/second/rockpapersessior/Scanner.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.second.rockpapersessior;
+
+import java.io.InputStream;
+
+/**
+ *
+ * @author HP
+ */
+class Scanner {
+
+    Scanner(InputStream in) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    int nextInt() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void nextLine() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    Object next() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
